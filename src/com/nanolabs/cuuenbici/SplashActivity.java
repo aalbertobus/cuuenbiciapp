c2aa1fc7e9a12315ef66e81ebd028dcc7af43c7a
Alberto Bustillos <aalbertobus@gmail.com>
2013-03-18 18:35:10 -0700

    Fb Login Added

--- src/com/nanolabs/cuuenbici/SplashActivity.java
+++ src/com/nanolabs/cuuenbici/SplashActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class SplashActivity extends Activity {
@@ -13,11 +15,14 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+      //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+      //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      	setContentView(R.layout.activity_splash);
         
         Thread timer = new Thread(){
         	public void run(){
-        		
         		try {
 					sleep(2000);
 				} catch (Exception e) {
--- src/com/nanolabs/cuuenbici/mainActivity.java
+++ src/com/nanolabs/cuuenbici/mainActivity.java
@@ -1,17 +1,55 @@
 package com.nanolabs.cuuenbici;
 
 import android.app.Activity;
+import com.facebook.*;
+import com.facebook.model.*;
+
+import android.view.Window;
+import android.widget.TextView;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class mainActivity extends Activity {
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);  // it points to activity_main.xml our second layout!
+		
+		// start Facebook Login
+		  Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+		    // callback when session changes state
+		    @Override
+		    public void call(Session session, SessionState state, Exception exception) {
+		    	if (session.isOpened()) {
+
+		            // make request to the /me API
+		            Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+		              // callback after Graph API response with user object
+		              @Override
+		              public void onCompleted(GraphUser user, Response response) {
+		                if (user != null) {
+		                  TextView welcome = (TextView) findViewById(R.id.welcome);
+		                  welcome.setText("Hello " + user.getName() + "! Your are in!!!");
+		                } 
+		              }
+		            });
+		        }
+		    }
+		  });	
 	}
 
-	
+	 @Override
+	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	      super.onActivityResult(requestCode, resultCode, data);
+	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	  }
 	
 }
