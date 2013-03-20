cd875435d941df4e912d895ae5bae2602825184f
aalbertobus <aalbertobus@gmail.com>
2013-03-20 14:22:02 -0700

    fb picture + menu and empty activities

--- src/com/nanolabs/cuuenbici/mainActivity.java
+++ src/com/nanolabs/cuuenbici/mainActivity.java
@@ -3,26 +3,24 @@ package com.nanolabs.cuuenbici;
 import android.app.Activity;
 import com.facebook.*;
 import com.facebook.model.*;
+import com.facebook.widget.ProfilePictureView;
 
+
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
-import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+
 import android.os.Bundle;
 
 public class mainActivity extends Activity {
 	
-	TextView welcome; 
-	TextView locationlabel;
-	boolean sesion;
+	 TextView welcome; 
+	Button ridebutton,searchridesbutton,logoutbutton;
+	 ProfilePictureView profilePictureView;
 	
-	
-	
-	
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -31,34 +29,17 @@ public class mainActivity extends Activity {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);  // it points to activity_main.xml our second layout!
 		
-		welcome = (TextView) findViewById(R.id.welcome);
-		locationlabel = (TextView) findViewById(R.id.txtlocation);
-	//--------------------------------------------LOCATION------------
 		
-		// Acquire a reference to the system Location Manager
-		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		welcome = (TextView) findViewById(R.id.userLabel);
+		profilePictureView = (ProfilePictureView) findViewById(R.id.selection_profile_pic);
+		ridebutton = (Button) findViewById(R.id.paseobutton);
+		searchridesbutton = (Button) findViewById(R.id.searchrideButton);
+		logoutbutton = (Button)findViewById(R.id.logoutbutton);
 		
-		// Define a listener that responds to location updates
-		LocationListener locationListener = new LocationListener() {
-		    public void onLocationChanged(Location location) {
-		      // Called when a new location is found by the network location provider.
-		      locationlabel.setText("lat: " + location.getLatitude() + " \n long: " + location.getLongitude());
-		    }
-
-		    public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-		    public void onProviderEnabled(String provider) {}
-
-		    public void onProviderDisabled(String provider) {}
-		  };
-
-		// Register the listener with the Location Manager to receive location updates
-		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-				
+		
 		
 		// start Facebook Login
 		  Session.openActiveSession(this, true, new Session.StatusCallback() {
-			  
 			  
 			 // callback when session changes state
 		    @Override
@@ -73,7 +54,46 @@ public class mainActivity extends Activity {
 		              public void onCompleted(GraphUser user, Response response) {
 		                if (user != null) {
 		                  
-		                  welcome.setText("Hello " + user.getName() + "!!!");
+		                	welcome.setText("Hello\n " + user.getName() + "!");
+		                  		                 
+		                	// view that in turn displays the profile picture.
+		                	profilePictureView.setProfileId(user.getId());
+		                	profilePictureView.setPresetSize(profilePictureView.CUSTOM);
+		                    
+		                    //BUTTON LISTENERS
+		                	ridebutton.setOnClickListener(new OnClickListener() {
+								
+								@Override
+								public void onClick(View arg0) {
+									Intent nextLayout = new Intent(getApplicationContext(),rideMap.class);
+									startActivity(nextLayout);
+									
+								}
+							});
+		                	
+		                	
+		                	searchridesbutton.setOnClickListener(new OnClickListener() {
+								
+								@Override
+								public void onClick(View v) {
+									Intent nextLayout = new Intent(getApplicationContext(),searchRides.class);
+									startActivity(nextLayout);
+									
+								}
+							});
+		                	
+		                	logoutbutton.setOnClickListener(new OnClickListener() {
+								
+								@Override
+								public void onClick(View v) {
+									// log out form facebook
+									
+								}
+							});
+		                	
+		                	
+		                	
+		                    
 		        
 		                } 
 		              }          
@@ -88,4 +108,9 @@ public class mainActivity extends Activity {
 	      super.onActivityResult(requestCode, resultCode, data);
 	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
 	  }	
+	 
+	 
+	 
+	 
+	 
 }
